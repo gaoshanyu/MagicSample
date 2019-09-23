74ab9bbc323754c7cf7e6a558db7f705093b0271
raniys <liaoyaoyu@kid17.com>
2019-09-23 17:12:55 +0800

    Initial Commit

---
+++ MagicSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MagicSample
+//
+//  Created by raniys on 2019/9/23.
+//  Copyright © 2019 raniys. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
