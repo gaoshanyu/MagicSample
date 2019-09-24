7f85e9db7a1ab869527f551dc2a5a204bd1da2d7
raniys <liaoyaoyu@kid17.com>
2019-09-24 17:18:53 +0800

    add hike sample

---
+++ MagicSample/Landmark/Drawing and Animation/HikeView.swift
@@ -0,0 +1,55 @@
+//
+//  HikeView.swift
+//  MagicSample
+//
+//  Created by raniys on 2019/9/24.
+//  Copyright © 2019 raniys. All rights reserved.
+//
+
+import SwiftUI
+
+struct HikeView: View {
+    var hike: Hike
+    @State private var showDetail = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                HikeGraph(hike: hike, path: \.elevation)
+                    .frame(width: 50, height: 30)
+                    .animation(nil)
+                
+                VStack(alignment: .leading) {
+                    Text(verbatim: hike.name)
+                        .font(.headline)
+                    Text(verbatim: hike.distanceText)
+                }
+                
+                Spacer()
+
+                Button(action: {
+                    self.showDetail.toggle()
+                }) {
+                    Image(systemName: "chevron.right.circle")
+                        .imageScale(.large)
+                        .rotationEffect(.degrees(showDetail ? 90 : 0))
+                        .padding()
+                }
+            }
+
+            if showDetail {
+                HikeDetailView(hike: hike)
+            }
+        }
+    }
+}
+
+struct HikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            HikeView(hike: hikeData[0])
+                .padding()
+            Spacer()
+        }
+    }
+}
